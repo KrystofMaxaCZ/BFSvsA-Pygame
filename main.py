d2b7f21363565287d570122f956449a1c1d4f521
KrystofMaxaCZ <krystof.maxa@gmail.com>
2026-02-08 22:03:22 +0100

    Uprava UI

--- main.py
+++ main.py
@@ -12,13 +12,21 @@ pygame.display.set_caption("Labyrinth")
 # Colors
 BACKGROUND_COLOR = (177, 191, 170)
 
-
+POLE_WIDTH = 10
+POLE_HEIGHT = 10
+WALL_COLOR = (250,0,0)
+BOARDER_COLOR = (0,0,0)
+AVAILABLE_COLUMN_COLOR = (0,250,0)
 # priprava prostredi 
 buttons = []
-for x in range(25):
-    for y in range(25):
-        button = Button(screen, (250,0,0), 100 + x * 15, 100 + y * 15, 10, 10, 1)
-        buttons.append(button)
+for x in range(POLE_WIDTH):
+    for y in range(POLE_HEIGHT):
+        if (x == 0) or (x == (POLE_WIDTH - 1)) or (y == 0) or (y==(POLE_HEIGHT - 1)):
+            button = Button(screen, BOARDER_COLOR, 200 + x * 35, 200 + y * 35, 25, 25, 3)
+            buttons.append(button)
+        else:
+            button = Button(screen, AVAILABLE_COLUMN_COLOR, 200 + x * 35, 200 + y * 35, 25, 25, 1)
+            buttons.append(button)
 # Game loop
 running = True
 while running:
@@ -31,10 +39,10 @@ while running:
             for button in buttons:
                 if button.is_clicked(event):
                     if button.type == 1:
-                        button.color = (0, 255, 0) # na zelenou
+                        button.color = WALL_COLOR # na zelenou
                         button.type = 0
                     elif button.type == 0:
-                        button.color = (255, 0, 0) # na cervenou zpatky
+                        button.color = AVAILABLE_COLUMN_COLOR # na cervenou zpatky
                         button.type = 1
     
     screen.fill(BACKGROUND_COLOR)
