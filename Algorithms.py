640b5872867316a0cc326ebfdfee3d4245bf8bc8
KrystofMaxaCZ <krystof.maxa@gmail.com>
2026-02-15 23:04:24 +0100

    Rozdeleni logiky kodu do trid, prehlednejsi kod Je tam rozdelany to porovnani dvou algoritmu, konkretne radky text_surf = node.font.render(node.text, True, (255, 255, 255))             text_rect = text_surf.get_rect(center=node.rect.center)             node.screen.blit(text_surf, text_rect)

---
+++ Algorithms.py
@@ -0,0 +1,212 @@
+from collections import deque
+from constants import *
+from Button import Button
+from queue import PriorityQueue
+
+
+class Algorithms:
+    def __init__(self, node_matrix):
+        self.node_matrix = node_matrix
+
+    def resetAlg(self):
+        for row in self.node_matrix:
+            for button in row:
+                if button.color == AVAILABLE_COLUMN_COLOR or button.color == WAY_COLOR:
+                    button.type = 0
+                    button.distance = 0
+                    button.predecessor = None
+                    button.color = AVAILABLE_COLUMN_COLOR
+                elif button.color == STARTING_NODE_COLOR:
+                    button.type = 0
+                    button.distance = 0
+                    button.predecessor = None
+                    button.color = STARTING_NODE_COLOR
+                elif button.color == WALL_COLOR:
+                    button.type = 1
+                    button.distance = 0
+                    button.predecessor = None
+
+    def wayBack(self, node: Button, starting_node):
+        """
+        Rekurzivne najde cestu z end_node do starting_node a obarvuje
+        
+        :param node: prvni zavolani je to end_node, dalsi jsou to predchudci, proto pojmenovani pouze node
+        :param starting_node: node ze ktereho jsme zacinali, tak tam chceme dorazit
+        """
+        if node.predecessor != starting_node:
+            node.predecessor.color = WAY_COLOR
+            self.wayBack(node.predecessor, starting_node)
+    def nodeScore(self, node, end_node) -> int:
+        """
+        Docstring for nodeScore
+        
+        :param node: Description
+        :param end_node: Description
+        :return: vrati score toho 
+        :rtype: int, score pro heruistiku
+        """
+        node_predecessor = node.predecessor
+        node.distance = node_predecessor.distance + 1
+        row_end = end_node.row
+        col_end = end_node.col
+        row_node = node.row
+        col_node = node.col
+
+        score = abs(row_end - row_node) + abs(col_end - col_node)
+        score += node.distance
+        return score
+    def BFS(self, starting_node, end_node):
+        """
+        Najde nejkratsi cestu mezi starting_node a end_node
+        
+        :param node_matrix: 2Dpole buttons/node 
+        :param starting_node: node ze ktereho zaciname
+        :param end_node: node kde koncime
+        """
+        # vycisteni po vice spustenich
+        # resetAlg()
+        # zacneme od startu
+        self.resetAlg()
+        queue = deque()
+        starting_node.distance = 0
+        queue.append(starting_node)
+        # indexy v 2D poli
+        row = starting_node.row
+        col = starting_node.col
+        pocet_pruchodu = 0
+
+        # starting_node = node_matrix[row, col]
+        found = False
+        while len(queue) != 0:
+            # nactu node z fronty
+            node = queue.popleft()
+            row = node.row
+            col = node.col
+            pocet_pruchodu += 1
+
+            node.text = str(node.distance)
+            text_surf = node.font.render(node.text, True, (255, 255, 255)) 
+            text_rect = text_surf.get_rect(center=node.rect.center)
+            node.screen.blit(text_surf, text_rect)
+            # pokud jsem v cili, koncim, nasel jsem cestu
+            if node == end_node:
+                print("Nasel jsem cestu")
+                found = True
+                self.wayBack(node, starting_node)
+                break
+            else:
+                # osetreni vystupu mimo pole
+                if (row - 1) >= 0:
+                    neighbour_node = self.node_matrix[row - 1][col]
+                    # type = 0 ... zelena availible space 
+                    if neighbour_node.type == 0:
+                        neighbour_node.predecessor = node
+                        neighbour_node.distance = node.distance + 1
+                        neighbour_node.type = 10
+                        queue.append(neighbour_node)
+                if (row + 1) < POLE_HEIGHT:
+                    neighbour_node = self.node_matrix[row + 1][col]
+                    if neighbour_node.type == 0:
+                        neighbour_node.predecessor = node
+                        neighbour_node.distance = node.distance + 1
+                        neighbour_node.type = 10
+                        queue.append(neighbour_node)
+                if (col - 1) >= 0:
+                    neighbour_node = self.node_matrix[row][col - 1]
+                    if neighbour_node.type == 0:
+                        neighbour_node.predecessor = node
+                        neighbour_node.distance = node.distance + 1
+                        neighbour_node.type = 10
+                        queue.append(neighbour_node)
+                if (col + 1) < POLE_WIDTH:
+                    neighbour_node = self.node_matrix[row][col + 1]
+                    if neighbour_node.type == 0:
+                        neighbour_node.predecessor = node
+                        neighbour_node.distance = node.distance + 1
+                        neighbour_node.type = 10
+                        queue.append(neighbour_node)    
+
+        if found == False:
+            print("cesta neexistuje")
+
+        print("Pocet navstivenych nodes = ", pocet_pruchodu)
+
+    def ASTAR(self, starting_node, end_node):
+        """
+        Docstring for ASTAR
+        velice podobne BFS
+        jediny rozdil je ze pouzivame prioritni frontu
+
+        """
+        # vycisteni po vice spustenich
+        # resetAlg()
+    # zacneme od startu
+        self.resetAlg()
+
+        pqueue = PriorityQueue()
+        starting_node.distance = 0
+        pqueue.put((0, starting_node))
+        # indexy v 2D poli
+        row = starting_node.row
+        col = starting_node.col
+        pocet_pruchodu = 0
+
+
+        found = False
+        while pqueue.empty() != True:
+            # nactu node z fronty
+            node = pqueue.get()[1]
+            row = node.row
+            col = node.col
+            pocet_pruchodu += 1
+
+            node.text = str(node.distance)
+            text_surf = node.font.render(node.text, True, (255, 255, 255)) 
+            text_rect = text_surf.get_rect(center=node.rect.center)
+            node.screen.blit(text_surf, text_rect)
+            # pokud jsem v cili, koncim, nasel jsem cestu
+            if node == end_node:
+                print("Nasel jsem cestu")
+                found = True
+                self.wayBack(node, starting_node)
+                break
+            else:
+                # osetreni vystupu mimo pole
+                if (row - 1) >= 0:
+                    neighbour_node = self.node_matrix[row - 1][col]
+                    # type = 0 ... zelena availible space 
+                    if neighbour_node.type == 0:
+                        neighbour_node.predecessor = node
+                        neighbour_node.type = 10
+                        neighbour_node_score = self.nodeScore(neighbour_node, end_node)
+
+                        pqueue.put((neighbour_node_score, neighbour_node))
+                if (row + 1) < POLE_HEIGHT:
+                    neighbour_node = self.node_matrix[row + 1][col]
+                    if neighbour_node.type == 0:
+                        neighbour_node.predecessor = node
+                        neighbour_node.type = 10
+                        neighbour_node_score = self.nodeScore(neighbour_node, end_node)
+
+                        pqueue.put((neighbour_node_score, neighbour_node))
+                if (col - 1) >= 0:
+                    neighbour_node = self.node_matrix[row][col - 1]
+                    if neighbour_node.type == 0:
+                        neighbour_node.predecessor = node
+                        neighbour_node.type = 10
+                        neighbour_node_score = self.nodeScore(neighbour_node, end_node)
+
+                        pqueue.put((neighbour_node_score, neighbour_node))
+                if (col + 1) < POLE_WIDTH:
+                    neighbour_node = self.node_matrix[row][col + 1]
+                    if neighbour_node.type == 0:
+                        neighbour_node.predecessor = node
+                        neighbour_node.type = 10
+                        neighbour_node_score = self.nodeScore(neighbour_node, end_node)
+
+                        pqueue.put((neighbour_node_score, neighbour_node))    
+
+        if found == False:
+            print("cesta neexistuje")
+
+        print("Pocet navstivenych nodes = ", pocet_pruchodu)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from Button import Button
 from collections import deque
 from queue import PriorityQueue
+from Algorithms import Algorithms
+from constants import *
 
 # https://www.geeksforgeeks.org/python/pygame-tutorial/
 # Initialize Pygame
@@ -12,22 +14,7 @@ resolution = (1500, 800)
 screen = pygame.display.set_mode(resolution)
 pygame.display.set_caption("Labyrinth")
 
-# Colors
-BACKGROUND_COLOR = (177, 191, 170)
 
-POLE_WIDTH = 10
-POLE_HEIGHT = 10
-WALL_COLOR = (179, 2, 2)
-BOARDER_COLOR = (71, 62, 44)
-WAY_COLOR = (20, 86, 199)
-AVAILABLE_COLUMN_COLOR = (0, 209, 42)
-STARTING_NODE_COLOR = (231, 245, 39)
-# MENU
-NOTACTIVE_COLOR = (100, 100, 100)
-ACTIVE_COLOR = (50, 200, 50)
-START_BUTTON_COLOR = (200, 150, 0)
-RESET_BUTTON_COLOR = (200, 50, 50)
-RESET_ALGO_COLOR = (160, 50, 50)
 
 button_bfs = Button(screen, 0, 0, ACTIVE_COLOR, 700, 100, 90, 50, None, "BFS")
 button_astar = Button(screen, 0, 0, NOTACTIVE_COLOR, 700, 170, 90, 50, None, "A*")
@@ -41,6 +28,9 @@ selected_algo = "BFS" # vychozi volba
 
 #PRIPRAVA PROSTREDI
 node_matrix = [] # 2D pole, list kde jsou prvky listy obashujici objkety typu Button
+
+# objekt pro algorimty 
+algs = Algorithms(node_matrix)
 
 # Vyroba policek pomoci objektu Button
 def prepare2DGrid():
@@ -69,221 +59,17 @@ def prepare2DGrid():
                 button = Button(screen,y,x, AVAILABLE_COLUMN_COLOR, 200 + x * 35, 200 + y * 35, 33, 33, 0)
                 row.append(button)
         node_matrix.append(row)
-
-def resetAlg():
-    for row in node_matrix:
-        for button in row:
-            if button.color == AVAILABLE_COLUMN_COLOR or button.color == WAY_COLOR:
-                button.type = 0
-                button.distance = 0
-                button.predecessor = None
-                button.color = AVAILABLE_COLUMN_COLOR
-            elif button.color == STARTING_NODE_COLOR:
-                button.type = 0
-                button.distance = 0
-                button.predecessor = None
-                button.color = STARTING_NODE_COLOR
-            elif button.color == WALL_COLOR:
-                button.type = 1
-                button.distance = 0
-                button.predecessor = None
-                
-                
             
 def newGame():
     global starting_node,end_node, node_matrix
 
-    node_matrix = []
+    node_matrix .clear()
     prepare2DGrid()
     starting_node = node_matrix[1][1]
     end_node = node_matrix[POLE_HEIGHT-2][POLE_WIDTH-2]
 
     return starting_node, end_node
 
-
-def wayBack(node: Button, starting_node):
-    """
-    Rekurzivne najde cestu z end_node do starting_node a obarvuje
-    
-    :param node: prvni zavolani je to end_node, dalsi jsou to predchudci, proto pojmenovani pouze node
-    :param starting_node: node ze ktereho jsme zacinali, tak tam chceme dorazit
-    """
-    if node.predecessor != starting_node:
-        node.predecessor.color = WAY_COLOR
-        wayBack(node.predecessor, starting_node)
-        
-# https://www.youtube.com/watch?v=HZ5YTanv5QE
-def BFS(node_matrix, starting_node, end_node):
-    """
-    Najde nejkratsi cestu mezi starting_node a end_node
-    
-    :param node_matrix: 2Dpole buttons/node 
-    :param starting_node: node ze ktereho zaciname
-    :param end_node: node kde koncime
-    """
-    # vycisteni po vice spustenich
-    # resetAlg()
-    # zacneme od startu
-    resetAlg()
-    queue = deque()
-    starting_node.distance = 10
-    queue.append(starting_node)
-    # indexy v 2D poli
-    row = starting_node.row
-    col = starting_node.col
-    pocet_pruchodu = 0
-
-    # starting_node = node_matrix[row, col]
-    found = False
-    while len(queue) != 0:
-        # nactu node z fronty
-        node = queue.popleft()
-        row = node.row
-        col = node.col
-        pocet_pruchodu += 1
-        # pokud jsem v cili, koncim, nasel jsem cestu
-        if node == end_node:
-            print("Nasel jsem cestu")
-            found = True
-            wayBack(node, starting_node)
-            break
-        else:
-            # osetreni vystupu mimo pole
-            if (row - 1) >= 0:
-                neighbour_node = node_matrix[row - 1][col]
-                # type = 0 ... zelena availible space 
-                if neighbour_node.type == 0:
-                    neighbour_node.predecessor = node
-                    neighbour_node.distance = node.distance + 1
-                    neighbour_node.type = 10
-                    queue.append(neighbour_node)
-            if (row + 1) < POLE_HEIGHT:
-                neighbour_node = node_matrix[row + 1][col]
-                if neighbour_node.type == 0:
-                    neighbour_node.predecessor = node
-                    neighbour_node.distance = node.distance + 1
-                    neighbour_node.type = 10
-                    queue.append(neighbour_node)
-            if (col - 1) >= 0:
-                neighbour_node = node_matrix[row][col - 1]
-                if neighbour_node.type == 0:
-                    neighbour_node.predecessor = node
-                    neighbour_node.distance = node.distance + 1
-                    neighbour_node.type = 10
-                    queue.append(neighbour_node)
-            if (col + 1) < POLE_WIDTH:
-                neighbour_node = node_matrix[row][col + 1]
-                if neighbour_node.type == 0:
-                    neighbour_node.predecessor = node
-                    neighbour_node.distance = node.distance + 1
-                    neighbour_node.type = 10
-                    queue.append(neighbour_node)    
-
-    if found == False:
-        print("cesta neexistuje")
-
-    print("Pocet navstivenych nodes = ", pocet_pruchodu)
-
-
-
-def nodeScore(node, end_node) -> int:
-    """
-    Docstring for nodeScore
-    
-    :param node: Description
-    :param end_node: Description
-    :return: vrati score toho 
-    :rtype: int, score pro heruistiku
-    """
-    node_predecessor = node.predecessor
-    node.distance = node_predecessor.distance + 1
-    row_end = end_node.row
-    col_end = end_node.col
-    row_node = node.row
-    col_node = node.col
-
-    score = abs(row_end - row_node) + abs(col_end - col_node)
-    score += node.distance
-    return score
-    
-def ASTAR(node_matrix, starting_node, end_node):
-    """
-    Docstring for ASTAR
-    velice podobne BFS
-    jediny rozdil je ze pouzivame prioritni frontu
-
-    """
-    # vycisteni po vice spustenich
-    # resetAlg()
- # zacneme od startu
-    resetAlg()
-
-    pqueue = PriorityQueue()
-    starting_node.distance = 0
-    pqueue.put((0, starting_node))
-    # indexy v 2D poli
-    row = starting_node.row
-    col = starting_node.col
-    pocet_pruchodu = 0
-
-    found = False
-    while pqueue.empty() != True:
-        # nactu node z fronty
-        node = pqueue.get()[1]
-        row = node.row
-        col = node.col
-        pocet_pruchodu += 1
-        # pokud jsem v cili, koncim, nasel jsem cestu
-        if node == end_node:
-            print("Nasel jsem cestu")
-            found = True
-            wayBack(node, starting_node)
-            break
-        else:
-            # osetreni vystupu mimo pole
-            if (row - 1) >= 0:
-                neighbour_node = node_matrix[row - 1][col]
-                # type = 0 ... zelena availible space 
-                if neighbour_node.type == 0:
-                    neighbour_node.predecessor = node
-                    neighbour_node.type = 10
-                    neighbour_node_score = nodeScore(neighbour_node, end_node)
-
-                    pqueue.put((neighbour_node_score, neighbour_node))
-            if (row + 1) < POLE_HEIGHT:
-                neighbour_node = node_matrix[row + 1][col]
-                if neighbour_node.type == 0:
-                    neighbour_node.predecessor = node
-                    neighbour_node.type = 10
-                    neighbour_node_score = nodeScore(neighbour_node, end_node)
-
-                    pqueue.put((neighbour_node_score, neighbour_node))
-            if (col - 1) >= 0:
-                neighbour_node = node_matrix[row][col - 1]
-                if neighbour_node.type == 0:
-                    neighbour_node.predecessor = node
-                    neighbour_node.type = 10
-                    neighbour_node_score = nodeScore(neighbour_node, end_node)
-
-                    pqueue.put((neighbour_node_score, neighbour_node))
-            if (col + 1) < POLE_WIDTH:
-                neighbour_node = node_matrix[row][col + 1]
-                if neighbour_node.type == 0:
-                    neighbour_node.predecessor = node
-                    neighbour_node.type = 10
-                    neighbour_node_score = nodeScore(neighbour_node, end_node)
-
-                    pqueue.put((neighbour_node_score, neighbour_node))    
-
-    if found == False:
-        print("cesta neexistuje")
-
-    print("Pocet navstivenych nodes = ", pocet_pruchodu)
-# # PREPARE GAME
-# prepare2DGrid()
-
-# starting_node = node_matrix[1][1]
-# end_node = node_matrix[POLE_HEIGHT-2][POLE_WIDTH-2]
 
 starting_node, end_node = newGame()
 # GAME LOOP
@@ -307,15 +93,15 @@ while running:
 
             elif button_start.is_clicked(event):
                 if selected_algo == "BFS":
-                    BFS(node_matrix, starting_node, end_node)
+                    algs.BFS(starting_node, end_node)
                 else:
-                    ASTAR(node_matrix, starting_node, end_node)
+                    algs.ASTAR(starting_node, end_node)
 
             elif button_reset.is_clicked(event):
                 starting_node, end_node = newGame()
 
             elif button_reset_alg.is_clicked(event):
-                resetAlg()
+                algs.resetAlg()
 
             else:
                 for row in node_matrix:
